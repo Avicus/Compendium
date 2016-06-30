7db2d2faa2293d1c4cec904bbf2b004349d77369
Austin Mayes <bookofnathan@gmail.com>
2016-06-30 18:38:57 -0500

    Custom CommandManager and Registration
    This allows for the dynamic registration and unregistration of commands.
    This also allows for command instances to be created at runtime and
    multiple commands to be tied to a single method.

---
+++ Bukkit/src/main/java/net/avicus/compendium/commands/AvicusCommandsManager.java
@@ -0,0 +1,81 @@
+package net.avicus.compendium.commands;
+
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandsManager;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class AvicusCommandsManager extends CommandsManager<CommandSender>
+{
+    @Override
+    public boolean hasPermission(CommandSender sender, String perm) {
+        return sender instanceof ConsoleCommandSender || sender.hasPermission(perm);
+    }
+
+    /**
+     * Registers a {@link Command} annotation with a method.
+     * This will cache the definition and method for use to provide {@link com.sk89q.minecraft.util.commands.SuggestionContext}'s and such by the framework.
+     * This runs the same code that would run by the internal framework if a class was supplied.
+     * The framework will see all of the dynamically registered commands as root-level commands.
+     *
+     * NOTE: This does not support nesting.
+     *
+     * @param method Method that the command will execute.
+     * @param command definiton of the command,
+     * @return the input definition for use in other methods.
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     */
+    public List<Command> registerCommandAnnotation(Method method, Command command) throws IllegalAccessException, InstantiationException {
+        Map<String, Method> map;
+        List<Command> registered = new ArrayList<Command>();
+
+        map = commands.get(null);
+
+        if(!(Void.TYPE.equals(method.getReturnType()) ||
+                List.class.isAssignableFrom(method.getReturnType()))) {
+            throw new RuntimeException("Command method " + method.getDeclaringClass().getName() + "#" + method.getName() +
+                    " must return either void or List<String>");
+        }
+
+        // Cache the aliases too
+        for (String alias : command.aliases()) {
+            map.put(alias, method);
+        }
+
+        final String commandName = command.aliases()[0];
+        final String desc = command.desc();
+
+        final String usage = command.usage();
+        if (usage.isEmpty()) {
+            descs.put(commandName, desc);
+        } else {
+            descs.put(commandName, usage + " - " + desc);
+        }
+
+        String help = command.help();
+        if (help.isEmpty()) {
+            help = desc;
+        }
+
+        final CharSequence arguments = getArguments(command);
+        for (String alias : command.aliases()) {
+            final String helpMessage = "/" + alias + " " + arguments + "\n\n" + help;
+            final String key = alias.replaceAll("/", "");
+            String previous = helpMessages.put(key, helpMessage);
+
+            if (previous != null && !previous.replaceAll("^/[^ ]+ ", "").equals(helpMessage.replaceAll("^/[^ ]+ ", ""))) {
+                helpMessages.put(key, previous + "\n\n" + helpMessage);
+            }
+        }
+
+
+        // Add the command to the registered command list for return
+        registered.add(command);
+
+        return registered;
+    }
+}
---
+++ Bukkit/src/main/java/net/avicus/compendium/commands/AvicusCommandsRegistration.java
@@ -0,0 +1,202 @@
+package net.avicus.compendium.commands;
+
+import com.sk89q.bukkit.util.CommandsManagerRegistration;
+import com.sk89q.bukkit.util.DynamicPluginCommand;
+import com.sk89q.minecraft.util.commands.CommandsManager;
+import com.sk89q.minecraft.util.commands.NestedCommand;
+import com.sk89q.util.ReflectionUtil;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandMap;
+import org.bukkit.plugin.Plugin;
+import sun.reflect.annotation.AnnotationParser;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class AvicusCommandsRegistration extends CommandsManagerRegistration {
+
+    public AvicusCommandsRegistration(Plugin plugin, CommandsManager commands) {
+        super(plugin, commands);
+    }
+
+    public AvicusCommandsRegistration(Plugin plugin, CommandExecutor executor, CommandsManager commands) {
+        super(plugin, executor, commands);
+    }
+
+    /**
+     * Unregister commands based on provided class
+     * This uses the same logic as the framework uses during registration, so nesting is supported.
+     *
+     * @param clazz
+     * @return
+     * @throws Exception
+     */
+    public boolean unregisterCommands(Class clazz) throws Exception {
+        CommandMap map = getCommandMap();
+
+        List<String> toRemove = new ArrayList<String>();
+        Map<String, Command> knownCommands = ReflectionUtil.getField(map, "knownCommands");
+        Set<String> aliases = ReflectionUtil.getField(map, "aliases");
+
+        if (knownCommands == null || aliases == null) {
+            return false;
+        }
+
+        List<com.sk89q.minecraft.util.commands.Command> commandsInClass = getCommandMethods(clazz, null, null);
+
+        List<String> aliasesToRemove = new ArrayList<>();
+
+        for (com.sk89q.minecraft.util.commands.Command command : commandsInClass) {
+            for (int i = 0; i < command.aliases().length; i++) {
+                aliasesToRemove.add(command.aliases()[i]);
+            }
+        }
+
+        for (Iterator<Command> i = knownCommands.values().iterator(); i.hasNext();) {
+            org.bukkit.command.Command cmd = i.next();
+            if (cmd instanceof DynamicPluginCommand && ((DynamicPluginCommand) cmd).getPlugin().equals(plugin) && ((DynamicPluginCommand) cmd).getOwner().equals(executor)) {
+                boolean remove = false;
+                for (String alias : cmd.getAliases()) {
+                    org.bukkit.command.Command aliasCmd = knownCommands.get(alias);
+                    if (cmd.equals(aliasCmd) && aliasesToRemove.contains(alias)) {
+                        remove = true;
+                        aliases.remove(alias);
+                        toRemove.add(alias);
+                    }
+                }
+
+                if (remove)
+                    i.remove();
+            }
+        }
+        for (String string : toRemove) {
+            knownCommands.remove(string);
+        }
+        return true;
+    }
+
+    /**
+     * Unregister a command
+     *
+     * @param command
+     * @return
+     * @throws Exception
+     */
+    public boolean unregisterCommand(com.sk89q.minecraft.util.commands.Command command) throws Exception {
+        CommandMap map = getCommandMap();
+
+        List<String> toRemove = new ArrayList<String>();
+        Map<String, Command> knownCommands = ReflectionUtil.getField(map, "knownCommands");
+        Set<String> aliases = ReflectionUtil.getField(map, "aliases");
+
+        if (knownCommands == null || aliases == null) {
+            return false;
+        }
+
+        List<String> aliasesToRemove = new ArrayList<>();
+
+        for (int i = 0; i < command.aliases().length; i++) {
+            aliasesToRemove.add(command.aliases()[i]);
+        }
+
+        for (Iterator<Command> i = knownCommands.values().iterator(); i.hasNext();) {
+            org.bukkit.command.Command cmd = i.next();
+            if (cmd instanceof DynamicPluginCommand && ((DynamicPluginCommand) cmd).getPlugin().equals(plugin) && ((DynamicPluginCommand) cmd).getOwner().equals(executor)) {
+                boolean remove = false;
+                for (String alias : cmd.getAliases()) {
+                    org.bukkit.command.Command aliasCmd = knownCommands.get(alias);
+                    if (cmd.equals(aliasCmd) && aliasesToRemove.contains(alias)) {
+                        remove = true;
+                        aliases.remove(alias);
+                        toRemove.add(alias);
+                    }
+                }
+
+                if (remove)
+                    i.remove();
+            }
+        }
+        for (String string : toRemove) {
+            knownCommands.remove(string);
+        }
+        return true;
+    }
+
+    private List<com.sk89q.minecraft.util.commands.Command> getCommandMethods(Class<?> cls, Method parent, Object obj) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        List<com.sk89q.minecraft.util.commands.Command> registered = new ArrayList<com.sk89q.minecraft.util.commands.Command>();
+
+        for (Method method : cls.getMethods()) {
+            if (!method.isAnnotationPresent(com.sk89q.minecraft.util.commands.Command.class)) {
+                continue;
+            }
+
+            if(!(Void.TYPE.equals(method.getReturnType()) ||
+                    List.class.isAssignableFrom(method.getReturnType()))) {
+                throw new RuntimeException("Command method " + method.getDeclaringClass().getName() + "#" + method.getName() +
+                        " must return either void or List<String>");
+            }
+
+
+            com.sk89q.minecraft.util.commands.Command cmd = method.getAnnotation(com.sk89q.minecraft.util.commands.Command.class);
+
+            // Add the command to the registered command list for return
+            registered.add(cmd);
+
+            // Look for nested commands -- if there are any, those have
+            // to be cached too so that they can be quickly looked
+            // up when processing commands
+            if (method.isAnnotationPresent(NestedCommand.class)) {
+                NestedCommand nestedCmd = method.getAnnotation(NestedCommand.class);
+
+                for (Class<?> nestedCls : nestedCmd.value()) {
+                    getCommandMethods(nestedCls, method, null);
+                }
+            }
+        }
+
+        if (cls.getSuperclass() != null) {
+            getCommandMethods(cls.getSuperclass(), parent, obj);
+        }
+
+        return registered;
+    }
+
+    /**
+     * Register a new command with the framework based on a method and a supplied command annotation.
+     * This will add the command to the framework internal cache and also add it to the {@link CommandMap}.
+     *
+     * @param method Method to be executed on command.
+     * @param definition Command definition,
+     * @return If the command registered successfully.
+     * @throws Exception
+     */
+    public boolean reqisterCommand(Method method, com.sk89q.minecraft.util.commands.Command definition) throws Exception {
+        AvicusCommandsManager commandsManager = (AvicusCommandsManager) commands;
+        return registerAll(commandsManager.registerCommandAnnotation(method, definition));
+    }
+
+    public com.sk89q.minecraft.util.commands.Command newCommand(Map<String, Object> params) {
+        return createAnnotationInstance(params, com.sk89q.minecraft.util.commands.Command.class);
+    }
+
+    // https://gist.github.com/thomasdarimont/7f0a31fcc0a65befef1f
+    private static <A extends Annotation> A createAnnotationInstance(Map<String, Object> customValues, Class<A> annotationType) {
+        Map<String, Object> values = new HashMap<>();
+
+        //Extract default values from annotation
+        for (Method method : annotationType.getDeclaredMethods()) {
+            if (values.containsKey(method.getName()))
+                continue;
+
+            values.put(method.getName(), method.getDefaultValue());
+        }
+
+        //Populate required values
+        values.putAll(customValues);
+
+        return (A) AnnotationParser.annotationForMap(annotationType, values);
+    }
+}
