b1ee231514b99220d1c885a4aaa97859dac08661
Austin Mayes <bookofnathan@gmail.com>
2016-11-05 03:26:07 -0500

    Back to 1.8 (duh)

--- Bukkit/src/main/java/net/avicus/compendium/sound/SoundType.java
+++ Bukkit/src/main/java/net/avicus/compendium/sound/SoundType.java
@@ -7,20 +7,21 @@ import org.bukkit.entity.Player;
 public enum SoundType {
 
     NONE(null),
-    DING(Sound.ENTITY_ARROW_HIT),
-    LEVEL_UP(Sound.ENTITY_PLAYER_LEVELUP),
-    BURP(Sound.ENTITY_PLAYER_BURP),
-    BARK(Sound.ENTITY_WOLF_GROWL),
-    LAUNCH(Sound.ENTITY_FIREWORK_LAUNCH),
-    SNARE(Sound.BLOCK_NOTE_SNARE),
-    ANVIL(Sound.BLOCK_ANVIL_LAND),
-    FIREWORK(Sound.ENTITY_FIREWORK_BLAST),
-    BASS(Sound.BLOCK_NOTE_BASEDRUM),
-    PIANO(Sound.BLOCK_NOTE_HARP),
-    PLING(Sound.BLOCK_NOTE_PLING),
-    PLOP(Sound.ENTITY_ITEM_PICKUP),
-    ENDERDRAGON(Sound.ENTITY_ENDERDRAGON_GROWL),
-    GOLEM_DEATH(Sound.ENTITY_IRONGOLEM_DEATH);
+    DING(Sound.ARROW_HIT),
+    LEVEL_UP(Sound.LEVEL_UP),
+    BURP(Sound.BURP),
+    BARK(Sound.WOLF_BARK),
+    LAUNCH(Sound.FIREWORK_LAUNCH),
+    SNARE(Sound.NOTE_SNARE_DRUM),
+    ANVIL(Sound.ANVIL_LAND),
+    FIREWORK(Sound.FIREWORK_BLAST),
+    BASS(Sound.NOTE_BASS),
+    PIANO(Sound.NOTE_PIANO),
+    PLING(Sound.NOTE_PLING),
+    PLOP(Sound.ITEM_PICKUP),
+    ENDERDRAGON(Sound.ENDERDRAGON_GROWL),
+    GOLEM_DEATH(Sound.IRONGOLEM_DEATH);
+    
     private final Sound sound;
 
     SoundType(Sound sound) {
