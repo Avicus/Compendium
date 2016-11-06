699055446fc4a1081be3afc134592caa9c6e3ce7
Austin Mayes <bookofnathan@gmail.com>
2016-11-05 21:46:58 -0500

    Add a few string utils
    thanks @ParaPenguin

--- Bukkit/src/main/java/net/avicus/compendium/utils/Strings.java
+++ Bukkit/src/main/java/net/avicus/compendium/utils/Strings.java
@@ -10,6 +10,7 @@ import org.joda.time.Seconds;
 import org.joda.time.format.PeriodFormatter;
 import org.joda.time.format.PeriodFormatterBuilder;
 
+import java.util.Collection;
 import java.util.List;
 
 public class Strings {
@@ -17,6 +18,43 @@ public class Strings {
         if (message == null)
             return null;
         return ChatColor.translateAlternateColorCodes('^', message);
+    }
+
+    /**
+     * Shorthand for listToEnglishCompound(list, "", "")
+     *
+     * @param list List of strings to compound.
+     * @return String version of the list of strings.
+     */
+    public static String listToEnglishCompound(Collection<String> list) {
+        return listToEnglishCompound(list, "", "");
+    }
+
+    /**
+     * Converts a list of strings to make a nice English list as a string.
+     *
+     * @param list List of strings to compound.
+     * @param prefix Prefix to add before each element in the resulting string.
+     * @param suffix Suffix to add after each element in the resulting string.
+     * @return String version of the list of strings.
+     */
+    public static String listToEnglishCompound(Collection<?> list, String prefix, String suffix) {
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        for(Object str : list) {
+            if(i != 0) {
+                if(i == list.size() - 1) {
+                    builder.append(" and ");
+                } else {
+                    builder.append(", ");
+                }
+            }
+
+            builder.append(prefix).append(str).append(suffix);
+            i++;
+        }
+
+        return builder.toString();
     }
 
     public static String removeColors(String message) {
