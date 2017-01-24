3d61266a17fd5f4a4e9e068486dba5bb31e12d5e
Austin Mayes <austin@avicus.net>
2017-01-24 17:59:55 -0600

    BossBar support for 1.11.12 clients

--- Bukkit/src/main/java/net/avicus/compendium/boss/BossBarManager.java
+++ Bukkit/src/main/java/net/avicus/compendium/boss/BossBarManager.java
@@ -50,7 +50,7 @@ public class BossBarManager implements Listener, Runnable {
             case 21:case 22:case 23:case 24:case 25:case 26:case 27:case 28:case 29:case 30:
             case 31:case 32:case 33:case 34:case 35:case 36:case 37:case 38:case 39:case 40:
             case 41:case 42:case 43:case 44:case 45:case 46:
-            case 47: // 1.8 - 1.8.9
+            case 47: // 1.7 - 1.8.9
                 return new LegacyBossBar(this, player);
             case 107: // 1.9
             case 108: // 1.9.1
@@ -58,6 +58,7 @@ public class BossBarManager implements Listener, Runnable {
             case 110: // 1.9.3, 1.9.4
             case 210: // 1.10 - 1.10.2
             case 315: // 1.11
+            case 316: // 1.11.1 - 1.11.12
                 return new ModernBossBar(player);
             default:
                 throw new RuntimeException("Could not resolve BossBar for protocol " + version + " for " + player.getUniqueId());
