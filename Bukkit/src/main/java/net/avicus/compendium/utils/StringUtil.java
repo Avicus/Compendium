abe51b2fa45930eaef770b85b467d83a5c813998
kashike <kashike@vq.lc>
2017-01-27 23:48:32 -0800

    Remove lore wrapping method

--- Bukkit/src/main/java/net/avicus/compendium/utils/StringUtil.java
+++ Bukkit/src/main/java/net/avicus/compendium/utils/StringUtil.java
@@ -1,12 +1,6 @@
 package net.avicus.compendium.utils;
 
-import org.apache.commons.lang3.SystemUtils;
-import org.apache.commons.lang3.text.WordUtils;
-import org.bukkit.ChatColor;
-import org.bukkit.inventory.meta.ItemMeta;
-
 import java.text.Normalizer;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
@@ -32,25 +26,5 @@ public final class StringUtil {
         return NON_LATIN.matcher(Normalizer.normalize(WHITESPACE.matcher(string).replaceAll("-"), Normalizer.Form.NFD))
             .replaceAll(REPLACEMENT)
             .toLowerCase(Locale.ENGLISH);
-    }
-
-    /**
-     * Wrap a long {@link ItemMeta} lore line and append it to a list of lore with
-     * last colors copied over.
-     *
-     * @param lore the result list
-     * @param length the length to wrap on
-     * @param string the string to wrap
-     */
-    public static void wrapLoreCopyColors(final List<String> lore, final int length, final String string) {
-        String previous = "";
-        for (String wrapped : WordUtils.wrap(string, length).split(SystemUtils.LINE_SEPARATOR)) {
-            if (!previous.isEmpty()) {
-                previous = ChatColor.getLastColors(previous);
-            }
-
-            lore.add(previous + wrapped);
-            previous = wrapped;
-        }
     }
 }
