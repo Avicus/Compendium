be3c921356cb5a257b4793c9eca4ce9617f09e6f
Austin Mayes <austin@avicus.net>
2017-02-01 20:48:00 -0600

    Move non-bukkit string utils to core

--- Bukkit/src/main/java/net/avicus/compendium/utils/Strings.java
+++ Bukkit/src/main/java/net/avicus/compendium/utils/Strings.java
@@ -4,15 +4,17 @@ import com.google.common.base.Preconditions;
 import net.avicus.compendium.text.Components;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.commons.lang3.text.WordUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.ChatPaginator;
 import org.joda.time.Duration;
 import org.joda.time.Seconds;
 import org.joda.time.format.PeriodFormatter;
 import org.joda.time.format.PeriodFormatterBuilder;
 
-import java.util.Collection;
 import java.util.List;
 
 // TODO - move methods into StringUtil if they work with strings, or their own class - this one is a mess
@@ -23,53 +25,8 @@ public class Strings {
         return ChatColor.translateAlternateColorCodes('^', message);
     }
 
-    /**
-     * Shorthand for listToEnglishCompound(list, "", "")
-     *
-     * @param list List of strings to compound.
-     * @return String version of the list of strings.
-     */
-    public static String listToEnglishCompound(Collection<String> list) {
-        return listToEnglishCompound(list, "", "");
-    }
-
-    /**
-     * Converts a list of strings to make a nice English list as a string.
-     *
-     * @param list List of strings to compound.
-     * @param prefix Prefix to add before each element in the resulting string.
-     * @param suffix Suffix to add after each element in the resulting string.
-     * @return String version of the list of strings.
-     */
-    public static String listToEnglishCompound(Collection<?> list, String prefix, String suffix) {
-        StringBuilder builder = new StringBuilder();
-        int i = 0;
-        for(Object str : list) {
-            if(i != 0) {
-                if(i == list.size() - 1) {
-                    if (list.size() > 2)
-                        builder.append(", and ");
-                    else
-                        builder.append(" and ");
-                } else {
-                    builder.append(", ");
-                }
-            }
-
-            builder.append(prefix).append(str).append(suffix);
-            i++;
-        }
-
-        return builder.toString();
-    }
-
     public static String removeColors(String message) {
         return message == null ? null : ChatColor.stripColor(message);
-    }
-
-    public static String toPercent(double proportion) {
-        int percent = (int) Math.floor(proportion * 100.0);
-        return percent + "%";
     }
 
     public static ChatColor toChatColor(DyeColor color) {
@@ -140,27 +97,6 @@ public class Strings {
         return periodFormatter.parsePeriod(text).toStandardDuration();
     }
 
-    public static String secondsToClock(int seconds) {
-        int hours = seconds / 3600;
-        int minutes = (seconds % 3600) / 60;
-        int secs = seconds % 60;
-
-        if (hours == 0)
-            return String.format("%02d:%02d", minutes, secs);
-
-        return String.format("%02d:%02d:%02d", hours, minutes, secs);
-    }
-
-    public static <T> String join(List<T> parts, String delimeter, Stringify<T> stringify) {
-        String text = "";
-        for (T part : parts) {
-            text += stringify.on(part);
-            if (parts.indexOf(part) != parts.size() - 1)
-                text += delimeter;
-        }
-        return text;
-    }
-
     public static String padChatMessage(BaseComponent message, String padChar, ChatColor padColor, ChatColor messageColor) {
         return padChatMessage(message.toPlainText(), padChar, padColor, messageColor);
     }
@@ -189,7 +125,24 @@ public class Strings {
         return com.google.common.base.Strings.repeat(padChar, (ChatPaginator.GUARANTEED_NO_WRAP_CHAT_PAGE_WIDTH - ChatColor.stripColor(text).length() - 2) / (padChar.length() * 2));
     }
 
-    public interface Stringify<T> {
-        String on(T object);
+    /**
+     * Wrap a long {@link ItemMeta} lore line and append it to a list of lore with
+     * last colors copied over.
+     *
+     * @param lore the result list
+     * @param length the length to wrap on
+     * @param string the string to wrap
+     */
+    public static void wrapLoreCopyColors(final List<String> lore, final int length, final String string) {
+        String previous = "";
+        for (String wrapped : WordUtils.wrap(string, length).split(SystemUtils.LINE_SEPARATOR)) {
+            if (!previous.isEmpty()) {
+                previous = ChatColor.getLastColors(previous);
+            }
+
+            lore.add(previous + wrapped);
+            previous = wrapped;
+        }
     }
+
 }
---
+++ Core/src/main/java/net/avicus/compendium/StringUtil.java
@@ -0,0 +1,103 @@
+package net.avicus.compendium;
+
+
+import java.text.Normalizer;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+public final class StringUtil {
+
+    /** A pattern for non-latin characters. */
+    private static final Pattern NON_LATIN = Pattern.compile("[^\\w-]");
+    /** A pattern for whitespace characters. */
+    private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
+    /** The string non-latin characters are replaced with. */
+    private static final String REPLACEMENT = ""; // Empty space!
+
+    private StringUtil() {
+    }
+
+    /**
+     * Transform a string into a slugged string.
+     *
+     * @param string the original string
+     * @return the slugged string
+     */
+    public static String slugify(String string) {
+        return NON_LATIN.matcher(Normalizer.normalize(WHITESPACE.matcher(string).replaceAll("-"), Normalizer.Form.NFD))
+            .replaceAll(REPLACEMENT)
+            .toLowerCase(Locale.ENGLISH);
+    }
+
+    /**
+     * Converts a list of strings to make a nice English list as a string.
+     *
+     * @param list List of strings to compound.
+     * @param prefix Prefix to add before each element in the resulting string.
+     * @param suffix Suffix to add after each element in the resulting string.
+     * @return String version of the list of strings.
+     */
+    public static String listToEnglishCompound(Collection<?> list, String prefix, String suffix) {
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        for(Object str : list) {
+            if(i != 0) {
+                if(i == list.size() - 1) {
+                    if (list.size() > 2)
+                        builder.append(", and ");
+                    else
+                        builder.append(" and ");
+                } else {
+                    builder.append(", ");
+                }
+            }
+
+            builder.append(prefix).append(str).append(suffix);
+            i++;
+        }
+
+        return builder.toString();
+    }
+
+    /**
+     * Shorthand for listToEnglishCompound(list, "", "")
+     *
+     * @param list List of strings to compound.
+     * @return String version of the list of strings.
+     */
+    public static String listToEnglishCompound(Collection<String> list) {
+        return listToEnglishCompound(list, "", "");
+    }
+
+    public static String toPercent(double proportion) {
+        int percent = (int) Math.floor(proportion * 100.0);
+        return percent + "%";
+    }
+
+    public static String secondsToClock(int seconds) {
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+
+        if (hours == 0)
+            return String.format("%02d:%02d", minutes, secs);
+
+        return String.format("%02d:%02d:%02d", hours, minutes, secs);
+    }
+
+    public static <T> String join(List<T> parts, String delimeter, Stringify<T> stringify) {
+        String text = "";
+        for (T part : parts) {
+            text += stringify.on(part);
+            if (parts.indexOf(part) != parts.size() - 1)
+                text += delimeter;
+        }
+        return text;
+    }
+
+    public interface Stringify<T> {
+        String on(T object);
+    }
+}
