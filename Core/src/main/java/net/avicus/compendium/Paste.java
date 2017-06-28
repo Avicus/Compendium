ad32bfd391b829ebe2df3a5b11b558b29a327f72
Austin Mayes <austin@avicus.net>
2017-06-28 15:23:47 -0500

    Use a new paste service

--- Core/src/main/java/net/avicus/compendium/Paste.java
+++ Core/src/main/java/net/avicus/compendium/Paste.java
@@ -1,9 +1,17 @@
 package net.avicus.compendium;
 
-import org.javalite.http.Http;
-import org.javalite.http.Post;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Paste {
+    private static String pasteURL = "https://paste.avicus.net/";
+
     private final String title;
     private final String author;
     private final String text;
@@ -15,9 +23,37 @@ public class Paste {
     }
 
     public String upload() {
-        String params = String.format("title=%s&name=%s&text=%s", this.title, this.author, this.text);
-        Post post = Http.post("https://paste.keenant.com/api/create", params);
-        post.header("User-Agent", "Java");
-        return post.text().replace("\n", "").replace("\r", "");
+        StringBuilder text = new StringBuilder();
+        text.append("Title: " + this.title);
+        text.append("\n");
+        text.append("Author: " + this.author);
+        text.append("\n\n");
+        text.append(this.text);
+
+        HttpURLConnection connection = null;
+        try {
+            //Create connection
+            URL url = new URL(pasteURL + "documents");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes(text.toString());
+            wr.flush();
+            wr.close();
+
+            //Get Response
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            return pasteURL + new JsonParser().parse(rd.readLine()).getAsJsonObject().get("key").getAsString();
+
+        } catch (IOException e) {
+            return null;
+        } finally {
+            if (connection == null) return null;
+            connection.disconnect();
+        }
     }
 }
