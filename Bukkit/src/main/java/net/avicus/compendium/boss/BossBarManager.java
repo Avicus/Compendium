b2eb8b61edeaec1977008f14aa342392a50fa2f2
Austin Mayes <austin@avicus.net>
2017-10-01 14:12:25 -0500

    Make bossbar check less fail-prone

--- Bukkit/src/main/java/net/avicus/compendium/boss/BossBarManager.java
+++ Bukkit/src/main/java/net/avicus/compendium/boss/BossBarManager.java
@@ -47,65 +47,13 @@ public class BossBarManager implements Listener, Runnable {
   @SuppressWarnings("unchecked")
   public BossBar create(@Nonnull final Player player) {
     final int version = Via.getAPI().getPlayerVersion(player);
-    switch (version) {
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case 10:
-      case 11:
-      case 12:
-      case 13:
-      case 14:
-      case 15:
-      case 16:
-      case 17:
-      case 18:
-      case 19:
-      case 20:
-      case 21:
-      case 22:
-      case 23:
-      case 24:
-      case 25:
-      case 26:
-      case 27:
-      case 28:
-      case 29:
-      case 30:
-      case 31:
-      case 32:
-      case 33:
-      case 34:
-      case 35:
-      case 36:
-      case 37:
-      case 38:
-      case 39:
-      case 40:
-      case 41:
-      case 42:
-      case 43:
-      case 44:
-      case 45:
-      case 46:
-      case 47: // 1.7 - 1.8.9
-        return new LegacyBossBar(this, player);
-      case 107: // 1.9
-      case 108: // 1.9.1
-      case 109: // 1.9.2
-      case 110: // 1.9.3, 1.9.4
-      case 210: // 1.10 - 1.10.2
-      case 315: // 1.11
-      case 316: // 1.11.1 - 1.11.12
-      case 335: // 1.12
-      case 338: // 1.12.1
-        return new ModernBossBar(player);
-      default:
-        throw new RuntimeException(
-            "Could not resolve BossBar for protocol " + version + " for " + player.getUniqueId());
-    }
+
+    if (version >= 4 && version <= 47) // 1.7-1.8.9
+      return new LegacyBossBar(this, player);
+    else if (version > 47) // 1.9+
+      return new ModernBossBar(player);
+    else
+      throw new RuntimeException(
+          "Could not resolve BossBar for protocol " + version + " for " + player.getUniqueId());
   }
 }
